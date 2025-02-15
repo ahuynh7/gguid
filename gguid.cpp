4681edea4affb3981602d3c3ff89ec90b75221b7
Anh Huynh <huynhanh8327@gmail.com>
2025-02-15 15:23:04 -0800

    substring the guid to exclude curly braces

--- gguid.cpp
+++ gguid.cpp
@@ -9,16 +9,24 @@ int main() {
 	//initialize guid
 	GUID guid;
 	HRESULT hr = CoCreateGuid(&guid);
+	const int length = 36;
 
 	if (SUCCEEDED(hr)) {
 		wchar_t* guid_string;
-		
+		wchar_t* formatted_guid_string = new wchar_t[length + 1];
+
 		if (StringFromCLSID(guid, &guid_string) == S_OK) {
+			//substring to remove curly braces
+			wcsncpy(formatted_guid_string, guid_string + 1, length);
+			formatted_guid_string[length] = L'\0';
+
 			//print to console
-			wcout << guid_string << endl;
+			wcout << formatted_guid_string << endl;
 
 			//free memory allocation
+			delete[] formatted_guid_string;
 			CoTaskMemFree(guid_string);
+
 		}
 		else { return 1; } 
 	}
--- gguid.h
+++ gguid.h
@@ -2,7 +2,9 @@
 // or project specific include files.
 
 #pragma once
+#pragma warning(disable : 4996)		//_CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
 #include <objbase.h>
+#include <string>
 #include <windows.h>

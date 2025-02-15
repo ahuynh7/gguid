e1f3c027d3b3439923df0df9e99568034c733d91
Anh Huynh <huynhanh8327@gmail.com>
2025-02-14 19:03:45 -0800

    Initial commit

---
+++ gguid.cpp
@@ -0,0 +1,16 @@
+﻿// gguid.cpp : Defines the entry point for the application.
+//
+
+#include "gguid.h"
+
+using namespace std;
+
+int main() {
+	cout << "gguid" << endl;
+
+	string a;
+	cin >> a;
+	cout << a << endl;
+
+	return 0;
+}
